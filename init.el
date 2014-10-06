ff5655b93301a0e9137b84273e07bcb8c15410bc
zzt <zengzetang@163.com>
2014-10-06 16:39:45 +0800

    horizontal frame

--- init.el
+++ init.el
@@ -182,3 +182,23 @@
 ;;(http://stackoverflow.com/questions/2081577/setting-emacs-split-to-horizontal)
 (setq split-height-threshold nil)
 (setq split-width-threshold 80)
+
+;;set "jk" as Escape
+;;(http://stackoverflow.com/questions/10569165/how-to-map-jj-to-esc-in-emacs-evil-mode?lq=1)
+(define-key evil-insert-state-map "j" #'cofi/maybe-exit)
+
+(evil-define-command cofi/maybe-exit ()
+  :repeat change
+  (interactive)
+  (let ((modified (buffer-modified-p)))
+    (insert "j")
+    (let ((evt (read-event (format "Insert %c to exit insert state" ?k)
+               nil 0.5)))
+      (cond
+       ((null evt) (message ""))
+       ((and (integerp evt) (char-equal evt ?k))
+    (delete-char -1)
+    (set-buffer-modified-p modified)
+    (push 'escape unread-command-events))
+       (t (setq unread-command-events (append unread-command-events
+                          (list evt))))))))
